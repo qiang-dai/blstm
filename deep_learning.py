6d80e07265fb3f0bb9122413c1eb6df11f539804
后端组-戴强 <qiang.dai@xinmei365.com>
2017-11-22 22:44:40 +0800

    可以部分预测

--- deep_learning.py
+++ deep_learning.py
@@ -453,13 +453,14 @@ _y_pred = sess.run(fetches, feed_dict)
 print('X_tt=',X_tt)
 print('y_tt=',y_tt)
 print('_y_pred=',_y_pred)
+print('X_tt, y_tt size:', X_tt.size(), y_tt.size())
+print('X_tt, y_tt shape:', X_tt.shape(), y_tt.shape())
 
 for i in range(2):
     x = X_tt[i]
     y = _y_pred[i*len(x):(i+1)*len(x)]
     x_index = [e for e in x if e > 0]
     y_index = [np.argmax(e) for e in y]
-    #y_index = [e for e in y_index if e > 0]
     print ("x:", x)
     print ("y:", y)
     print ("x_index:", x_index)
@@ -470,147 +471,4 @@ for i in range(2):
     print (word_list)
     print (label_list)
 
-#nodes = [dict(zip(['s','b','m','e'], each[1:])) for each in _y_pred]
-#print('nodes=',nodes)
 
-# # 利用 labels（即状态序列）来统计转移概率
-# # 因为状态数比较少，这里用 dict={'I_tI_{t+1}'：p} 来实现
-# # A统计状态转移的频数
-# A = {
-#       'sb':0,
-#       'ss':0,
-#       'be':0,
-#       'bm':0,
-#       'me':0,
-#       'mm':0,
-#       'eb':0,
-#       'es':0
-#      }
-#
-# # zy 表示转移概率矩阵
-# zy = dict()
-# for label in labels:
-#     for t in range(len(label) - 1):
-#         key = label[t] + label[t+1]
-#         A[key] += 1.0
-#
-# zy['sb'] = A['sb'] / (A['sb'] + A['ss'])
-# zy['ss'] = 1.0 - zy['sb']
-# zy['be'] = A['be'] / (A['be'] + A['bm'])
-# zy['bm'] = 1.0 - zy['be']
-# zy['me'] = A['me'] / (A['me'] + A['mm'])
-# zy['mm'] = 1.0 - zy['me']
-# zy['eb'] = A['eb'] / (A['eb'] + A['es'])
-# zy['es'] = 1.0 - zy['eb']
-# keys = sorted(zy.keys())
-# print( 'the transition probability: ')
-# for key in keys:
-#     print( key, zy[key])
-#
-# zy = {i:np.log(zy[i]) for i in zy.keys()}
-#
-# def viterbi(nodes):
-#     """
-#     维特比译码：除了第一层以外，每一层有4个节点。
-#     计算当前层（第一层不需要计算）四个节点的最短路径：
-#        对于本层的每一个节点，计算出路径来自上一层的各个节点的新的路径长度（概率）。保留最大值（最短路径）。
-#        上一层每个节点的路径保存在 paths 中。计算本层的时候，先用paths_ 暂存，然后把本层的最大路径保存到 paths 中。
-#        paths 采用字典的形式保存（路径：路径长度）。
-#        一直计算到最后一层，得到四条路径，将长度最短（概率值最大的路径返回）
-#     """
-#     paths = {'b': nodes[0]['b'], 's':nodes[0]['s']} # 第一层，只有两个节点
-#     for layer in range(1, len(nodes)):  # 后面的每一层
-#         paths_ = paths.copy()  # 先保存上一层的路径
-#         # node_now 为本层节点， node_last 为上层节点
-#         paths = {}  # 清空 path
-#         for node_now in nodes[layer].keys():
-#             # 对于本层的每个节点，找出最短路径
-#             sub_paths = {}
-#             # 上一层的每个节点到本层节点的连接
-#             for path_last in paths_.keys():
-#                 if path_last[-1] + node_now in zy.keys(): # 若转移概率不为 0
-#                     sub_paths[path_last + node_now] = paths_[path_last] + nodes[layer][node_now] + zy[path_last[-1] + node_now]
-#             # 最短路径,即概率最大的那个
-#             sr_subpaths = pd.Series(sub_paths)
-#             sr_subpaths = sr_subpaths.sort_values()  # 升序排序
-#             node_subpath = sr_subpaths.index[-1]  # 最短路径
-#             node_value = sr_subpaths[-1]   # 最短路径对应的值
-#             # 把 node_now 的最短路径添加到 paths 中
-#             paths[node_subpath] = node_value
-#     # 所有层求完后，找出最后一层中各个节点的路径最短的路径
-#     sr_paths = pd.Series(paths)
-#     sr_paths = sr_paths.sort_values()  # 按照升序排序
-#     return sr_paths.index[-1]  # 返回最短路径（概率值最大的路径）
-#
-#
-# def text2ids(text):
-#     """把字片段text转为 ids."""
-#     words = list(text)
-#     ids = list(word2id[words])
-#     if len(ids) >= max_len:  # 长则弃掉
-#         print( u'输出片段超过%d部分无法处理' % (max_len) )
-#         return ids[:max_len]
-#     ids.extend([0]*(max_len-len(ids))) # 短则补全
-#     ids = np.asarray(ids).reshape([-1, max_len])
-#     return ids
-#
-#
-# def simple_cut(text):
-#     """对一个片段text（标点符号把句子划分为多个片段）进行预测。"""
-#     if text:
-#         text_len = len(text)
-#         X_batch = text2ids(text)  # 这里每个 batch 是一个样本
-#         fetches = [y_pred]
-#         feed_dict = {X_inputs:X_batch, lr:1.0, batch_size:1, keep_prob:1.0}
-#         _y_pred = sess.run(fetches, feed_dict)[0][:text_len]  # padding填充的部分直接丢弃
-#         nodes = [dict(zip(['s','b','m','e'], each[1:])) for each in _y_pred]
-#         tags = viterbi(nodes)
-#         words = []
-#         for i in range(len(text)):
-#             if tags[i] in ['s', 'b']:
-#                 words.append(text[i])
-#             else:
-#                 words[-1] += text[i]
-#         return words
-#     else:
-#         return []
-#
-#
-# def cut_word(sentence):
-#     """首先将一个sentence根据标点和英文符号/字符串划分成多个片段text，然后对每一个片段分词。"""
-#     not_cuts = re.compile(u'([0-9\da-zA-Z ]+)|[。，、？！.\.\?,!]')
-#     result = []
-#     start = 0
-#     for seg_sign in not_cuts.finditer(sentence):
-#         result.extend(simple_cut(sentence[start:seg_sign.start()]))
-#         result.append(sentence[seg_sign.start():seg_sign.end()])
-#         start = seg_sign.end()
-#     result.extend(simple_cut(sentence[start:]))
-#     return result
-#
-# sentence = u'人们常说生活是一部教科书'
-# result = cut_word(sentence)
-# rss = ''
-# for each in result:
-#     rss = rss + each + ' / '
-# print( rss)
-#
-# # 例一
-# sentence = u'人们思考问题往往不是从零开始的。就好像你现在阅读这篇文章一样，你对每个词的理解都会依赖于你前面看到的一些词，\
-#       而不是把你前面看的内容全部抛弃了，忘记了，再去理解这个单词。也就是说，人们的思维总是会有延续性的。'
-# result = cut_word(sentence)
-# rss = ''
-# for each in result:
-#     rss = rss + each + ' / '
-# print( rss)
-#
-# # 例二
-# sentence = u'天舟一号是我国自主研制的首艘货运飞船，由于它只运货，不送人，所以被形象地称为太空“快递小哥”。\
-#     它采用两舱式结构，直径较小的是推进舱，直径较大的为货物舱。其最大直径达到3.35米，飞船全长10.6米，载荷能力达到了6.5吨，\
-#     满载货物时重13.5吨。如果此次满载的话，它很可能将成为中国发射进入太空的质量最大的有效载荷。甚至比天宫二号空间实验室还大，\
-#     后者全长10.4米，直径同为3.35米，质量为8.6吨。'
-# result = cut_word(sentence)
-# rss = ''
-# for each in result:
-#     rss = rss + each + ' / '
-# print( rss)
