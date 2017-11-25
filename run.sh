168d36b1fc0bb6ec3e72e4cc415c6e118de6cb7e
后端组-戴强 <qiang.dai@xinmei365.com>
2017-11-25 20:16:48 +0800

    处理小说

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #python3 step01_format_sentence.py > t
-#python3 step11_save_index.py 2000000
+#python3 step11_save_index.py 1000000
 python3 step21_deep_learning.py 1000
 python3 step31_check_result.py 100 96
