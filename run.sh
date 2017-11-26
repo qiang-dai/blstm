801f1e35d6ed29ce5c11f6738a4e9c5c7d770815
后端组-戴强 <qiang.dai@xinmei365.com>
2017-11-26 16:35:49 +0800

    修复模型中的单词个数错误

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #python3 step01_format_sentence.py > t
 #python3 step11_save_index.py 1000000
 python3 step04_format_multi_punc.py raw_data/en_punctuation_recommend_train_100W 10000000 raw_data/res.txt
-python3 step11_save_index.py raw_data/res.txt 1000000
+python3 step11_save_index.py raw_data/res.txt 10000000
 python3 step21_deep_learning.py 1000
 python3 step31_check_result.py 100 96
