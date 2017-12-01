aead8fe4789be755a3ab1e7839b6865e0827eebf
后端组-戴强 <qiang.dai@xinmei365.com>
2017-12-01 17:22:45 +0800

    跑10轮

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ python3 step04_format_multi_punc.py raw_data/3.txt 1000000000 raw_data/res.txt
 #python3 step04_format_multi_punc.py raw_data/news.txt 1000000000 raw_data/res.txt
 python3 step05_slip_window.py raw_data/res.txt 1000000000 raw_data/split_window.txt
 python3 step11_save_index.py raw_data/split_window.txt 500000000
-python3 step21_deep_learning.py 1000
+python3 step21_deep_learning.py 10
 #python3 step31_check_result.py 100 96
