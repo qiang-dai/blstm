eec8f6de883ad913ad7856c2b98277ddcf6b57a8
后端组-戴强 <qiang.dai@xinmei365.com>
2017-12-04 18:53:18 +0800

    split优化

--- step05_slip_window.py
+++ step05_slip_window.py
@@ -11,6 +11,7 @@ import codecs
 import pyIO
 import tools
 import datetime
+import pickle
 
 
 ###读入所有内容,依次拼起来
@@ -63,8 +64,20 @@ def combine_line(filename, threshold_line_cnt, result_name, punc_list):
 ###每隔 32 个单词就处理一下
 def save_fixed_letter(total_list, result_name, punc_list):
     line_list = []
+    word_list = []
+    label_list= []
+
+    word2id = {}
+    tag2id = {}
+    id2word = {}
+    id2tag = {}
+    ###映射表
+    for i, punc in enumerate(punc_list):
+        tag2id[punc] = i
+        id2tag[i] = punc
+
     for i in range(len(total_list)):
-        if i%1000 == 0:
+        if i%100000 == 0:
             print ('i:', i, len(total_list))
 
         end = i + punctuation.get_timestep_size()
@@ -74,16 +87,50 @@ def save_fixed_letter(total_list, result_name, punc_list):
         #line_list.append(' '.join(total_list[i:end]) + '\n')
         ###这里只取第 N/2 - 1 个标点符号进行预测
         res = []
+
+        tmp_word_list = []
+        tmp_label_list= []
         for index,item in enumerate(total_list[i:end]):
+            word,punc = item.split('/')
             if index == punctuation.get_timestep_size()/2 - 1:
                 res.append(item)
             else:
-                word,punc = item.split('/')
                 res.append(word + '/' + punc_list[0])
+
+            ###2个对应的表
+            if word not in word2id:
+                cnt = len(word2id) + 1
+                word2id[word] = cnt
+                id2word[cnt] = word
+
+            tmp_word_list.append(word2id[word])
+            if index == punctuation.get_timestep_size()/2 - 1:
+                tmp_label_list.append(tag2id[punc])
+            else:
+                tmp_label_list.append(tag2id[punc_list[0]])
+            #     res.append(item)
+            # else:
+            #     word,punc = item.split('/')
+            #     res.append(word + '/' + punc_list[0])
         line_list.append(' '.join(res) + '\n')
+        word_list.append(tmp_word_list)
+        label_list.append(tmp_label_list)
 
     pyIO.save_to_file('\n'.join(line_list), result_name)
     print (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),line_list[:30])
+
+    ###写数据
+    X = np.asarray(word_list)
+    y = np.asarray(label_list)
+    with open('data/data2.pkl', 'wb') as outp:
+        pickle.dump(X, outp)
+        pickle.dump(y, outp)
+        pickle.dump(word2id, outp)
+        pickle.dump(id2word, outp)
+        pickle.dump(tag2id, outp)
+        pickle.dump(id2tag, outp)
+    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 'save over')
+
 
 if __name__ == '__main__':
 
