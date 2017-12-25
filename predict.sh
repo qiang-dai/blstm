4f097bade6990fdbe35a9830a11487b53d4c4414
后端组-戴强 <qiang.dai@xinmei365.com>
2017-12-25 10:37:09 +0800

    预测

---
+++ predict.sh
@@ -0,0 +1,4 @@
+python3 step41_file_predict.py valid_kika.txt >> c1
+python3 step41_file_predict.py valid_subtitle.txt >> c2
+python3 step41_file_predict.py valid_twitter.txt >> c3
+python3 step41_file_predict.py valid_wiki.txt >> c4
